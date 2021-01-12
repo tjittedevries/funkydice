01a18b9617d0943ebb5f337918ca99d0e9a7b648
Tjitte de Vries <post@tjittedevries.com>
2021-01-12 12:44:58 +0100

    compile typescript with rollup

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,104 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "funkydice", {
-  enumerable: true,
-  get: function () {
-    return _funkydice.funkydice;
-  }
-});
+Object.defineProperty(exports, '__esModule', { value: true });
 
-var _funkydice = require("./funkydice");
+// interface RollsResult {
+//   result: number;
+//   rolls: RollResult[];
+// }
+function die(faces) {
+    return {
+        faces: faces,
+        result: Math.floor(Math.random() * faces) + 1,
+    };
+}
+
+function dice(amount, faces) {
+    var results = [];
+    for (var i = 0; i < amount; i++) {
+        results.push(die(faces));
+    }
+    return results;
+}
+
+function modify(value, modifier) {
+    if (isNaN(modifier.value))
+        return value;
+    switch (modifier.type) {
+        case "-":
+            return value - modifier.value;
+        case "+":
+            return value - modifier.value;
+        default:
+            return value;
+    }
+}
+
+function roll(command) {
+    var rolls = dice(command.amount, command.faces);
+    var total = rolls.reduce(function (acc, current) { return acc + current.result; }, 0);
+    var totalModified = command.modifier
+        ? modify(total, command.modifier)
+        : total;
+    return {
+        command: command,
+        rolls: rolls,
+        total: total,
+        totalModified: totalModified,
+    };
+}
+
+// Example commandString:
+// * 1d4,2d6-7
+// * 2d6-7
+// * 1d4
+function convertStringToCommands(commandString) {
+    var seperatedCommandStrings = extractCommandsFromString(commandString);
+    var commands = [];
+    seperatedCommandStrings.forEach(function (value) {
+        var result = extractCommandFromString(value);
+        if (result) {
+            commands.push(result);
+        }
+    });
+    return commands;
+}
+// @todo should this return an array of type Command?
+function extractCommandsFromString(commandString) {
+    return commandString.split(",");
+}
+function extractCommandFromString(commandString) {
+    var regex = /^([1-9]\d*)d([1-9]\d*)([+-]\d+)?$/gim;
+    var regexResults = regex.exec(commandString);
+    if (!regexResults)
+        return undefined;
+    return {
+        amount: parseInt(regexResults[1], 10),
+        faces: parseInt(regexResults[2], 10),
+        modifier: extractModifierFromString(regexResults[3]),
+    };
+}
+function extractModifierFromString(modifierString) {
+    var regex = /^([+-])(\d+)$/gim;
+    var regexResults = regex.exec(modifierString);
+    if (!regexResults)
+        return undefined;
+    var modifierType = undefined;
+    if (regexResults[1] === "-") {
+        modifierType = "-";
+    }
+    else if (regexResults[1] === "+") {
+        modifierType = "+";
+    }
+    return {
+        type: modifierType,
+        value: parseInt(regexResults[2], 10),
+    };
+}
+
+function funkydice(commandString) {
+    var commands = convertStringToCommands(commandString);
+    return commands.map(function (command) { return roll(command); });
+}
+
+exports.funkydice = funkydice;
--- src/convertStringToCommands.ts
+++ src/convertStringToCommands.ts
@@ -24,7 +24,7 @@ export function convertStringToCommands(commandString: string): Command[] {
   return commands;
 }
 
-// @todo should this return an array of type Command? 
+// @todo should this return an array of type Command?
 export function extractCommandsFromString(commandString: string): string[] {
   return commandString.split(",");
 }
