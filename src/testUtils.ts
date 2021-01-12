717910c0153875293dc0733aa3af025260770d50
Tjitte de Vries <post@tjittedevries.com>
2021-01-12 12:59:33 +0100

    cleanup

--- src/testUtils.ts
+++ src/testUtils.ts
@@ -1,3 +1,5 @@
+import { expect } from "@jest/globals";
+
 export function expectBetween(value: number, max: number): void {
   expect(value).toBeGreaterThanOrEqual(1);
   expect(value).toBeLessThanOrEqual(max);
