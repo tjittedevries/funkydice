f446803bb28294cc4db72b007fd324f2d1cdb344
Tjitte de Vries <post@tjittedevries.com>
2021-01-12 11:38:57 +0100

    updated compile to exclude test files from dist

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _stringDice = require("./stringDice");
-
-const writeMessage = message => {
-  console.log(message);
-};
-
-writeMessage("Hello, World!" + (0, _stringDice.stringDice)());
